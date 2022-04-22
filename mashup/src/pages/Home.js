9236da259fa083c4fae79ae7d58fa3a8e70f60bd
Greg Munro <gregmunro@Websy-Greg.local>
2022-04-22 18:17:19 +0200

    nav

--- mashup/src/pages/Home.js
+++ mashup/src/pages/Home.js
@@ -8,13 +8,14 @@ const Home = () => {
       <h1>Dashboard</h1>
 
       <div className="card">
-  <div className="card-body" width="20px">
-    <h5 className="card-title">Product</h5>
-    <p className="card-text">Select a product from this carousel.</p>
-    <a href="#" className="btn btn-primary">Link</a>
-  </div>
-</div>
-
+        <div className="card-body" width="20px">
+          <h5 className="card-title">Product</h5>
+          <p className="card-text">Select a product from this carousel.</p>
+          <a href="#" className="btn btn-primary">
+            Link
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
