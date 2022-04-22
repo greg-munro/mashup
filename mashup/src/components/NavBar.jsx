026121bc75efdf07655c6428cb7baf198ef28c03
Greg Munro <gregmunro@Websy-Greg.local>
2022-04-22 17:52:46 +0200

    homepage boot

--- mashup/src/components/NavBar.jsx
+++ mashup/src/components/NavBar.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/top-products">Top Products</Link>
-      <Link to="/map">Map button</Link>
+      <nav class="navbar navbar-light bg-light">
+        <Link to="/">Home</Link>
+        <Link to="/top-products">Top Products</Link>
+        <Link to="/map">Map</Link>
+      </nav>
     </div>
   );
 };
--- mashup/src/pages/Home.js
+++ mashup/src/pages/Home.js
@@ -6,6 +6,15 @@ const Home = () => {
     <div>
       <NavBar />
       <h1>Dashboard</h1>
+
+      <div className="card">
+  <div className="card-body" width="20px">
+    <h5 className="card-title">Product</h5>
+    <p className="card-text">Select a product from this carousel.</p>
+    <a href="#" className="btn btn-primary">Link</a>
+  </div>
+</div>
+
     </div>
   );
 };
